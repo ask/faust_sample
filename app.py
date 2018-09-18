8cbb3eee59cd20f24e004a01f0e64d2631031edd
Wes Connell <wes@raptorlabs.io>
2018-09-17 22:56:37 -0700

    Removing local pythonpath append.

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 # proj/app.py
 import faust
-import sys
-sys.path.append('/opt/capture')
 
 app = faust.App(
     'proj',
     version=1,
     autodiscover=True,
-    origin='proj.capture'   # imported name for this project (import proj -> "proj")
+    origin='proj'   # imported name for this project (import proj -> "proj")
 )
 
 def main() -> None:
